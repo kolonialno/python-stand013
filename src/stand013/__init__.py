aa1dea993d5e7ba262b7daaf8295998af53687c6
Stein Magnus Jodal <stein.magnus.jodal@oda.com>
2021-06-17 19:22:27 +0200

    Get version from pyproject.toml

--- src/stand013/__init__.py
+++ src/stand013/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import PackageNotFoundError, version  # type: ignore
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
