dbfea0a12dea221ad9c36b004fccd7689c1b5e99
Stein Magnus Jodal <stein.magnus.jodal@oda.com>
2021-06-17 20:18:18 +0200

    tests: Test ORDRSP validation

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,12 +1,21 @@
 from pathlib import Path
 
+import pytest
+
 from stand013.validation import DocumentType
 
 EXAMPLES_DIR = Path(__file__).parent / "examples"
 
 
-def test_validate_order() -> None:
-    path = EXAMPLES_DIR / "orders.xml"
+@pytest.mark.parametrize(
+    "doc",
+    [
+        "orders.xml",
+        "ordrsp.xml",
+    ],
+)
+def test_validate_document(doc: str) -> None:
+    path = EXAMPLES_DIR / doc
 
     document_type = DocumentType.detect(path)
     document_type.xml_schema.validate(str(path))
