00fc3782a66c4a42271f74f18034409c8c406411
Stein Magnus Jodal <stein.magnus.jodal@oda.com>
2021-06-17 14:56:08 +0200

    validation: Create XML schema instances

--- src/stand013/validation.py
+++ src/stand013/validation.py
@@ -1,1 +1,15 @@
+import pathlib
+
 import xmlschema
+
+SCHEMA_DIR = pathlib.Path(__file__).parent / "schemas"
+
+order_schema = xmlschema.XMLSchema(
+    str(SCHEMA_DIR / "STAND013-Order_Interchange_v1p1.xsd")
+)
+order_response_schema = xmlschema.XMLSchema(
+    str(SCHEMA_DIR / "STAND013-OrderResponse_Interchange_v1p1.xsd")
+)
+delivery_note_schema = xmlschema.XMLSchema(
+    str(SCHEMA_DIR / "STAND013-DeliveryNote_Interchange_v1p1.xsd")
+)
