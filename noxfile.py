c65c10328d52f011b6103321fb6c0ee18f214491
Stein Magnus Jodal <stein.magnus.jodal@oda.com>
2022-01-05 15:28:06 +0100

    Test on Python 3.10

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 locations = ["src", "tests", "noxfile.py"]
 
 
-@nox.session(python=["3.7", "3.8", "3.9"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def tests(session):
     """Run the test suite."""
     args = session.posargs or ["--cov"]
@@ -13,7 +13,7 @@ def tests(session):
     session.run("pytest", *args)
 
 
-@nox.session(python=["3.7", "3.8", "3.9"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def flake8(session):
     """Lint using flake8."""
     args = session.posargs or locations
@@ -21,7 +21,7 @@ def flake8(session):
     session.run("flake8", *args)
 
 
-@nox.session(python=["3.7", "3.8", "3.9"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def mypy(session):
     """Type-check using mypy."""
     args = session.posargs or locations
